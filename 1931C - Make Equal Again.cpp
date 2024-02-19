dd34ced3556e08a0a85903245093bb280a6a71e4
81vm3 <p3g4sus@foxmail.com>
2024-02-19 15:51:31 +0800

    优化

--- 1931C - Make Equal Again.cpp
+++ 1931C - Make Equal Again.cpp
@@ -10,55 +10,48 @@ int main() {
 	int t, n;
 
 	cin >> t;
-	vector<vector<int>> v(t);
-
-	for (int i = 0; i < t; i++) {
-		cin >> n;
-		v[i].resize(n);
-		for (int j = 0; j < n; j++) {
-			cin >> v[i][j];
-		}
-	}
+	int v[200001];
+	int ans[10001];
 
 	int s, e, ptr1, ptr2;
 	bool f1, f2;
+
 	for (int i = 0; i < t; i++) {
-		if (v[i].size() == 1)
-			cout << 0;
-		else {
-			ptr1 = 0;
-			ptr2 = v[i].size() - 1;
-		
-			s = v[i][ptr1];
-			e = v[i][ptr2];
-			f1 = f2 = true;
+		cin >> n;
+		for (int j = 0; j < n; j++)
+			cin >> v[j];
 
-			while (f1 || f2) {
-				if (v[i][ptr1] == s && ptr1 != v[i].size() - 1) {
-					ptr1++;
-				} else {
-					f1 = false;
-				}
-
-				if (v[i][ptr2] == e && ptr2 != 0) {
-					ptr2--;
-				} else {
-					f2 = false;
-				}
-			}
-
-			if (s != e) {
-				//减去从头/尾最大连续数的个数
-				//cout << "mx " << max(ptr1, (int)v[i].size() - ptr2 - 1) << endl;
-				cout << v[i].size() - max(ptr1, (int)v[i].size() - ptr2 - 1); 
-			} else {
-				cout << max(0, ptr2 - ptr1 + 1);
-			}
+		if (n == 1) {
+			ans[i] = 0;
+			continue;
 		}
-		
-		if (i != t -1) 
-			cout << endl;
+
+		ptr1 = 0;
+		ptr2 = n - 1;
+
+		s = v[ptr1];
+		e = v[ptr2];
+		f1 = f2 = true;
+
+		while (f1 || f2) {
+			if (v[ptr1] == s && ptr1 != n - 1)
+				ptr1++;
+			else 
+				f1 = false;
+
+			if (v[ptr2] == e && ptr2 != 0) 
+				ptr2--;
+			else
+				f2 = false;
+		}
+
+		ans[i] = s == e ? max(0, ptr2 - ptr1 + 1) : n - max(ptr1, n - ptr2 - 1);
 	}
 
+	for (int i = 0; i < t; i++) {
+		cout << ans[i];
+		if (i != t - 1)
+			cout << endl;
+	}
 	return 0;
 }
